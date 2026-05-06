acba89f3476c574771b889ea90bd7ca69865f667
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2026-05-06 10:19:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ if "sentences" not in st.session_state:
 # -----------------------------
 st.title("🌍 Entraînement multilingue")
 
-langue = st.selectbox("Langue", ["it", "de"])
+langue = st.selectbox("Langue", ["Italien", "Allemand"])
 jour = st.selectbox("Article", ["Aujourd'hui", "Hier"])
 
 articles = get_articles(langue)
