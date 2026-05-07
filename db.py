e33732441587b0f79edafe2c40d481007dbc229a
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2026-05-07 09:20:02 +0200

    Update db.py

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ def get_connection():
     return psycopg2.connect(
         host=st.secrets["DB_HOST"],
         database=st.secrets["DB_NAME"],
-        user=st.secrets["DB_USER"],
+        username=st.secrets["DB_USER"],
         password=st.secrets["DB_PASSWORD"],
         port=st.secrets["DB_PORT"]
     )
